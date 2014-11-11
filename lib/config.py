2467fb94cb1ed46e60833f6a7422c3ddee7e2ec2
Martin Feller <martinfeller73@gmail.com>
2014-11-12 11:32:14 +1300

    run headless

--- lib/config.py
+++ lib/config.py
@@ -2,6 +2,6 @@
 url = 'http://www.eresearch.auckland.ac.nz'
 
 # selenium webdriver / browser. choose PhantomJS for headless tests
-driver = 'webdriver.Firefox()'
-#driver = 'webdriver.PhantomJS()'
+#driver = 'webdriver.Firefox()'
+driver = 'webdriver.PhantomJS()'
 
