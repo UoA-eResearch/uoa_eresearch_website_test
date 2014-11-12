bcaddd52af8ab6114b4180f1bdcdb64fc259f848
Martin Feller <martinfeller73@gmail.com>
2014-11-12 18:33:40 +1300

    initial commit

---
+++ lib/util.py
@@ -0,0 +1,22 @@
+import sys
+import shlex
+import subprocess
+
+def execute(command_and_args, error_on_stderr=True, error_on_nonzero_rc=True):
+  ''' Run a system call '''
+  try:
+    process = subprocess.Popen(shlex.split(command_and_args), shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    (stdout, stderr) = process.communicate()
+    rc = process.returncode
+  except:
+    raise Exception("Failed to execute '%s': %s" % (command_and_args, sys.exc_info()[1]))
+
+  if rc != 0 and error_on_nonzero_rc:
+    raise Exception('\'%s\' returned exit code %d. stderr: %s' % (command_and_args, rc, stderr))
+
+  if stderr != "" and error_on_stderr:
+    raise Exception('Error executing command \'%s\': Got non-empty stderr: %s' % (command_and_args, stderr))
+
+  return (stdout, stderr, rc)
+
+
